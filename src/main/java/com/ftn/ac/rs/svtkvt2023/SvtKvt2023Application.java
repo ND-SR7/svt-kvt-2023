86f6079691e3efa4c6231c97aa9e88d8c0f68557
ND-SR7 <davogic.sr7.2021@uns.ac.rs>
2023-04-17 17:43:53 +0200

    Inicijalizacija projekta
    Projekat inicijalizovan sa svim potrebnim dependency-ima

---
+++ src/main/java/com/ftn/ac/rs/svtkvt2023/SvtKvt2023Application.java
@@ -0,0 +1,13 @@
+package com.ftn.ac.rs.svtkvt2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvtKvt2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvtKvt2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ftn/ac/rs/svtkvt2023/SvtKvt2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.ac.rs.svtkvt2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvtKvt2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
