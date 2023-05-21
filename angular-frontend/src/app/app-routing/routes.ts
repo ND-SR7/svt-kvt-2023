1673d4665f9e80f784e335c8843bbf9e0e7f1c81
ND-SR7 <davogic.sr7.2021@uns.ac.rs>
2023-05-21 17:20:51 +0200

    Quality of Life #1
    -Dodat prikaz naziva aplikacije u browser tab-u
    -Dodat link za promenu lozinke u navbar
    -Popravljen bug gde se moralo kliknuti na tekst u dugmetu za prelaz na registraciju da bi radilo
    -Dodat favicon od aplikacije

--- angular-frontend/src/app/app-routing/routes.ts
+++ angular-frontend/src/app/app-routing/routes.ts
@@ -6,8 +6,8 @@ import { RegisterComponent } from '../user/register/register.component';
 import { ChangePasswordComponent } from '../user/change-password/change-password.component';
 
 export const routes :Routes = [
-	{path: 'groups', component: GroupListComponent},
-	{path: 'users/login', component: LoginComponent},
-	{path: 'users/register', component: RegisterComponent},
-	{path: 'users/change-password', component: ChangePasswordComponent}
+	{path: 'groups', component: GroupListComponent, title: 'Social Network'},
+	{path: 'users/login', component: LoginComponent, title: 'Social Network'},
+	{path: 'users/register', component: RegisterComponent, title: 'Social Network'},
+	{path: 'users/change-password', component: ChangePasswordComponent, title: 'Social Network'}
 ];
