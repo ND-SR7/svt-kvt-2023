f8c80122e6bc0f40112ebea5c49123f964d01286
ND-SR7 <davogic.sr7.2021@uns.ac.rs>
2023-06-18 12:40:08 +0200

    Dodata logika za EditGroup
    Dodata TypeScript logika za komponentu EditGroup

--- angular-frontend/src/app/group/edit-group/edit-group.component.ts
+++ angular-frontend/src/app/group/edit-group/edit-group.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/user/services/authentication.service';
+import { Group } from "src/app/group/model/group.model";
 
 @Component({
   selector: 'app-edit-group',
   templateUrl: './edit-group.component.html',
   styleUrls: ['./edit-group.component.css']
 })
-export class EditGroupComponent {
+export class EditGroupComponent implements OnInit {
 
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) { 
+    this.form = this.fb.group({
+      name: [null, Validators.required],
+      description: [null, Validators.required],
+      creationDate: [null, Validators.required],
+      isSuspended: [null, Validators.required],
+      suspendedReason: [null]
+    })
+   }
+
+   ngOnInit(): void {
+     
+   }
+
+   submit() {
+    const group: Group = new Group;
+		group.name = this.form.value.name;
+		group.description = this.form.value.description;
+    group.creationDate = this.form.value.creationDate;
+    group.isSuspended = this.form.value.isSuspended;
+    group.suspendedReason = this.form.value.suspendedReason;
+// todo
+		// this.authenticationService.login(auth).subscribe(
+		// 	result => {
+		// 		window.alert('Successful login!');
+		// 		localStorage.setItem('user', JSON.stringify(result));
+		// 		this.router.navigate(['groups']);
+		// 	},
+		// 	error => {
+    //     window.alert('An error occurred!');
+		// 		console.log(error);
+		// 	}
+		// );
+  }
 }
