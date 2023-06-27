cd79a68d62b3a4a1a856763b43121a9b5c67b25b
ND-SR7 <davogic.sr7.2021@uns.ac.rs>
2023-06-27 20:54:49 +0200

    Prikaz displayName-a
    -Dodat prikaz displayName-a u interfejsu, za korisnike koji ga imaju podešenog
    -Ažuriran User model

--- angular-frontend/src/app/user/model/user.model.ts
+++ angular-frontend/src/app/user/model/user.model.ts
@@ -3,26 +3,32 @@ export class User {
     username: string;
     password: string;
     email: string;
-    lastLogin: Date;
+    lastLogin: string;
     firstName: string;
     lastName: string;
+    displayName: string;
+    description: string;
 
     constructor(obj: {
         _id?: number,
         username?: string,
         password?: string,
         email?: string,
-        lastLogin?: Date,
+        lastLogin?: string,
         firstName?: string,
-        lastName?: string
+        lastName?: string,
+        displayName?: string,
+        description?: string
     } = {}) {
         this._id = obj._id || null as unknown as number;
         this.username = obj.username || null as unknown as string;
         this.password = obj.password || null as unknown as string;
         this.email = obj.email || null as unknown as string;
-        this.lastLogin = obj.lastLogin || null as unknown as Date;
+        this.lastLogin = obj.lastLogin || null as unknown as string;
         this.firstName = obj.firstName || null as unknown as string;
         this.lastName = obj.lastName || null as unknown as string;
+        this.displayName = obj.displayName || null as unknown as string;
+        this.description = obj.description || null as unknown as string;
     }
 
     get id() {
