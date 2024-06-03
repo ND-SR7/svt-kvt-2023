4b413ea2010f057b80db5aa616dbe3ad1592830a
ND-SR7 <davogic.sr7.2021@uns.ac.rs>
2024-06-03 20:38:21 +0200

    Dodat paket sa custom exceptions

---
+++ java-backend/src/main/java/com/ftn/ac/rs/svtkvt2023/exception/LoadingException.java
@@ -0,0 +1,8 @@
+package com.ftn.ac.rs.svtkvt2023.exception;
+
+public class LoadingException extends RuntimeException {
+
+    public LoadingException(String msg) {
+        super(msg);
+    }
+}
---
+++ java-backend/src/main/java/com/ftn/ac/rs/svtkvt2023/exception/StorageException.java
@@ -0,0 +1,8 @@
+package com.ftn.ac.rs.svtkvt2023.exception;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String msg) {
+        super(msg);
+    }
+}
